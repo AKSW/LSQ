e40ce1a7b9607ab19d45c587c278514babed9544
Claus Stadler <RavenArkadon@googlemail.com>
2016-05-06 14:33:04 +0200

    wip

--- lsq-cli/src/main/java/org/aksw/simba/lsq/cli/main/NestedResource.java
+++ lsq-cli/src/main/java/org/aksw/simba/lsq/cli/main/NestedResource.java
@@ -1,23 +1,26 @@
 package org.aksw.simba.lsq.cli.main;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
 
 public class NestedResource {
     protected String str;
     protected Resource current = null;
+    protected Model model;
     protected NestedResource parent;
 
-    public NestedResource(String str) {
-        this(null, str);
+    public NestedResource(Model model, String str) {
+        this(null, model, str, null);
     }
 
     public NestedResource(Resource current) {
-        this(null, current.getURI());
-        this.current = current;
+        this(null, current.getModel(), current.getURI(), current);
     }
 
-    public NestedResource(NestedResource parent, String str) {
+    public NestedResource(NestedResource parent, Model model, String str, Resource current) {
         this.parent = parent;
+        this.model = model;
         this.str = str;
+        this.current = current;
     }
 
     /**
@@ -27,7 +30,7 @@ public class NestedResource {
      */
     public Resource get() {
         if(current == null) {
-            Resource result = current.getModel().createResource(str);
+            Resource result = model.createResource(str);
             return result;
         }
         return current;
@@ -40,7 +43,7 @@ public class NestedResource {
     public NestedResource nest(String name) {
         NestedResource result = name.isEmpty()
                 ? this
-                : new NestedResource(this, str + name);
+                : new NestedResource(this, model, str + name, null);
 
         return result;
     }
