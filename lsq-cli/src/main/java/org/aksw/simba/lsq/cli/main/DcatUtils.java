0bcceeb36083f9a53a8db4347598b7bbf50c87a3
Claus Stadler <RavenArkadon@googlemail.com>
2016-06-01 12:47:32 +0200

    wip

---
+++ lsq-cli/src/main/java/org/aksw/simba/lsq/cli/main/DcatUtils.java
@@ -0,0 +1,12 @@
+package org.aksw.simba.lsq.cli.main;
+
+import java.util.List;
+
+import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.apache.jena.rdf.model.Resource;
+
+public class DcatUtils {
+    public List<Resource> findDistributions(QueryExecutionFactory qef, String searchString) {
+        return null;
+    }
+}
