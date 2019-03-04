a7957f423684a9e7d5abaaaed30808b8aceb3297
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-03-04 19:55:46 +0100

    Added lsq-probe cli tool to probe a file's log format against a registry

--- lsq-core/src/main/java/org/aksw/simba/lsq/core/LsqUtils.java
+++ lsq-core/src/main/java/org/aksw/simba/lsq/core/LsqUtils.java
@@ -10,11 +10,14 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.aksw.commons.util.bean.PropertyUtils;
@@ -56,16 +59,63 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.StringUtils;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
 import com.google.common.collect.Streams;
+import com.google.common.collect.TreeMultimap;
 
 public class LsqUtils {
 	private static final Logger logger = LoggerFactory.getLogger(LsqUtils.class);
 
+
+	public static List<String> probeLogFormat(String resource) {
+		ResourceLoader loader = new FileSystemResourceLoader();		
+		Map<String, Function<InputStream, Stream<Resource>>> registry = LsqUtils.createDefaultLogFmtRegistry();
+		
+		Multimap<Long, String> report = probeLogFormat(registry, loader, resource);
+		
+		List<String> result = Streams.stream(report.asMap().entrySet().iterator())
+			.filter(e -> e.getKey() != 0)
+			.limit(1)
+			.map(Entry::getValue)
+			.flatMap(Collection::stream)
+			.collect(Collectors.toList());
+
+		return result;
+	}
+
+	public static Multimap<Long, String> probeLogFormat(Map<String, Function<InputStream, Stream<Resource>>> registry, ResourceLoader loader, String filename) {
+		org.springframework.core.io.Resource resource = loader.getResource(filename);
+		
+		// succcessCountToFormat
+		Multimap<Long, String> result = TreeMultimap.create(Ordering.natural().reverse(), Ordering.natural());
+		
+		for(Entry<String, Function<InputStream, Stream<Resource>>> entry : registry.entrySet()) {
+			String formatName = entry.getKey();
+			Function<InputStream, Stream<Resource>> fn = entry.getValue();
+			
+			try(InputStream in = resource.getInputStream()) {
+				//List<Resource> items =
+				long count = fn.apply(in)
+						.limit(1000)
+						.filter(r -> !r.hasProperty(LSQ.processingError))
+						.count();
+						//.collect(Collectors.toList());
+				
+				result.put(count, formatName);
+			} catch(Exception e) {
+				// Ignore
+			}
+		}
+
+		return result;
+	}
 
 	public static void applyDefaults(LsqConfigImpl config) {
 		// Set the default log format registry if no other has been set
