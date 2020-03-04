74b817e44a9fc5a5d88c89aae60016f4f64bbbb7
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-03-04 17:20:11 +0100

    Towards improving the rdfize command

--- lsq-cli/src/main/java/org/aksw/simba/lsq/cli/main/cmd/CmdLsqRdfize.java
+++ lsq-cli/src/main/java/org/aksw/simba/lsq/cli/main/cmd/CmdLsqRdfize.java
@@ -17,6 +17,18 @@ public class CmdLsqRdfize {
 	@Parameter(names={"-n", "--namespaces"}, description="Namespace prefix sources")
 	public List<String> prefixSources = new ArrayList<>();
 
+	@Parameter(names={"--no-merge"}, description="Do not merge inverted output by graph")
+	public boolean noMerge = false;
+	
+	@Parameter(names={"--no-hash"}, description="For privacy, host names are hashed by default. This option prevents it.")
+	public boolean noHostHash = false;
+
+	@Parameter(names={"--salt"}, description="Prepend the given salt to host names before hashing - otherwise a random uuid will be used")
+	public String hostSalt = null;
+
+	@Parameter(names={"-s", "--slim"}, description="Slim output only retains query, hostname and timestamp")
+	public boolean slimMode = false;
+
 	@Parameter(description="log sources")
 	public List<String> nonOptionArgs = new ArrayList<>();
 }
