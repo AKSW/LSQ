770b7e4504fd36834980df7d87a8f2f589d23abc
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-06-04 16:47:49 +0200

    removed unused import

--- lsq-core/src/main/java/org/aksw/simba/lsq/model/LsqQuery.java
+++ lsq-core/src/main/java/org/aksw/simba/lsq/model/LsqQuery.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 import org.aksw.jena_sparql_api.mapper.annotation.Iri;
 import org.aksw.jena_sparql_api.mapper.annotation.ResourceView;
-import org.aksw.jena_sparql_api.mapper.annotation.ToString;
 import org.aksw.simba.lsq.vocab.LSQ;
 import org.apache.jena.rdf.model.Resource;
 
