d36c6d94ae6020e3904cdb1759fe0163ee273389
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-07-27 21:13:19 +0200

    NPE fix in lsq plugin loading

--- lsq-core/src/main/java/org/aksw/simba/lsq/jena/plugin/JenaPluginLsq.java
+++ lsq-core/src/main/java/org/aksw/simba/lsq/jena/plugin/JenaPluginLsq.java
@@ -4,11 +4,14 @@ import org.aksw.jena_sparql_api.mapper.proxy.JenaPluginUtils;
 import org.aksw.simba.lsq.model.LsqQuery;
 import org.aksw.simba.lsq.spinx.model.SpinQueryEx;
 import org.apache.jena.sys.JenaSubsystemLifecycle;
-import org.topbraid.spin.vocabulary.SP;
+import org.apache.jena.sys.JenaSystem;
 
 public class JenaPluginLsq
     implements JenaSubsystemLifecycle {
 
+    static {
+        JenaSystem.init();
+    }
     public void start() {
         init();
     }
@@ -19,9 +22,8 @@ public class JenaPluginLsq
 
 
     public static void init() {
+        //SP.init(BuiltinPersonalities.model);
         // Noop to load spin personalities
-        @SuppressWarnings("unused")
-        Object noop = SP.Aggregation;
 
         JenaPluginUtils.scan(LsqQuery.class);
         JenaPluginUtils.scan(SpinQueryEx.class);
