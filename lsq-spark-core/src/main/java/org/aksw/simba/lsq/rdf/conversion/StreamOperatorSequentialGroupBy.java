bde5aec52abf6f4d9ccaa2b1928861a97f764b47
Claus Stadler <RavenArkadon@googlemail.com>
2021-07-02 15:33:43 +0200

    collapse operator now implemented as a spliterator

--- lsq-spark-core/src/main/java/org/aksw/simba/lsq/rdf/conversion/StreamOperatorSequentialGroupBy.java
+++ lsq-spark-core/src/main/java/org/aksw/simba/lsq/rdf/conversion/StreamOperatorSequentialGroupBy.java
@@ -1,20 +1,27 @@
 package org.aksw.simba.lsq.rdf.conversion;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.Spliterators.AbstractSpliterator;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Streams;
 
 
@@ -166,6 +173,18 @@ class SequentialGroupBySpec<T, K, V>
         return create(getGroupKey, Objects::equals, accCtor, (acc, item) -> { accAdd.accept(acc, item); return acc; });
     }
 
+    public static <T, K, V> SequentialGroupBySpec<T, K, V> create(
+            Function<? super T, ? extends K> getGroupKey,
+            Supplier<? extends V> accCtor,
+            BiConsumer<? super V, ? super T> accAdd) {
+        return create(getGroupKey, Objects::equals, k -> accCtor.get(), (acc, item) -> { accAdd.accept(acc, item); return acc; });
+    }
+
+    public static <T, K> SequentialGroupBySpec<T, K, List<T>> createList(
+            Function<? super T, ? extends K> getGroupKey) {
+        return create(getGroupKey, () -> new ArrayList<T>(), (acc, item) -> { acc.add(item); });
+    }
+
 
     /**
      * Create method with the following characteristics:
@@ -213,12 +232,17 @@ public class StreamOperatorSequentialGroupBy<T, K, V>
 
     /** Low-level iterator-based transformation */
     public Iterator<Entry<K, V>> transform(Iterator<T> input) {
-        return new OperatorImpl(input).getOutput();
+        return new OperatorImpl(input).getDownstream();
     }
 
     /** Stream-based transformation - relies on the iterator-based one */
     public Stream<Entry<K, V>> transform(Stream<T> input) {
         return Streams.stream(transform(input.sequential().iterator()));
+    }
+
+
+    public Spliterator<Entry<K, V>> transform(Spliterator<T> upstream) {
+        return new SpliteratorImpl(upstream);
     }
 
 //    interface IteratorDelegate<T>
@@ -243,17 +267,24 @@ public class StreamOperatorSequentialGroupBy<T, K, V>
     public class OperatorImpl
         extends AccumulatorBase
     {
-        protected Iterator<T> input;
-        protected Iterator<Entry<K, V>> output;
+        protected Iterator<T> upstream;
+        protected Iterator<Entry<K, V>> downstream;
 
-        public OperatorImpl(Iterator<T> input) {
-            super();
-            this.input = input;
-            this.output = new InternalIterator();
+        // The last seen item
+        protected T item;
+
+        public T getLastSeenItem() {
+            return item;
         }
 
-        public Iterator<Entry<K, V>> getOutput() {
-            return output;
+        public OperatorImpl(Iterator<T> upstream) {
+            super();
+            this.upstream = upstream;
+            this.downstream = new InternalIterator();
+        }
+
+        public Iterator<Entry<K, V>> getDownstream() {
+            return downstream;
         }
 
         class InternalIterator extends AbstractIterator<Entry<K, V>> {
@@ -263,8 +294,11 @@ public class StreamOperatorSequentialGroupBy<T, K, V>
 
                 Entry<K, V> result = null;
 
-                while (input.hasNext() && result == null) {
-                    T item = input.next();
+                // It is crucial to check for a (non-null) result first before calling
+                // upstream.hasNext() which may drain another item which then
+                // gets lost in the spliterator code!
+                while (result == null && upstream.hasNext()) {
+                    item = upstream.next();
 
                     currentKey = getGroupKey.apply(item);
 
@@ -273,14 +307,12 @@ public class StreamOperatorSequentialGroupBy<T, K, V>
                         priorKey = currentKey;
                         currentAcc = accCtor.apply(accNum, currentKey);
 
-                        // Objects.requireNonNull(currentAcc, "Got null for an accumulator");
                         ++accNum;
                     } else if (!groupKeyCompare.test(priorKey, currentKey)) {
 
                         result = new SimpleEntry<>(priorKey, currentAcc);
 
                         currentAcc = accCtor.apply(accNum, currentKey);
-                        // Objects.requireNonNull(currentAcc, "Got null for an accumulator");
                         ++accNum;
                     }
 
@@ -308,14 +340,163 @@ public class StreamOperatorSequentialGroupBy<T, K, V>
     }
 
 
+
+    public class SpliteratorImpl extends AbstractSpliterator<Entry<K, V>> {
+
+        /** Aggregator used to drain one group from the rhs of a split */
+        protected StreamOperatorSequentialGroupBy<T, K, List<T>> listAggregator =
+            StreamOperatorSequentialGroupBy.create(SequentialGroupBySpec.createList(getGroupKey));
+
+        protected Iterator<T> headItem;
+        protected Spliterator<T> upstream;
+        protected Iterator<T> tailItems;
+
+        protected Iterator<T> iteratorView;
+        protected Iterator<Entry<K, V>> aggIteratorView;
+
+
+        protected SpliteratorImpl(Spliterator<T> upstream) {
+            this(Collections.<T>emptyList().iterator(), upstream, Collections.<T>emptyList().iterator());
+        }
+
+        protected SpliteratorImpl(Iterator<T> headItem, Spliterator<T> upstream, Iterator<T> tailItems) {
+            super(upstream.estimateSize(), Spliterator.ORDERED);
+            this.headItem = headItem;
+            this.upstream = upstream;
+            this.tailItems = tailItems;
+
+            updateIteratorViews();
+        }
+
+        protected void updateIteratorViews() {
+            this.iteratorView = Iterators.concat(headItem, Spliterators.iterator(upstream), tailItems);
+            this.aggIteratorView = new OperatorImpl(iteratorView).getDownstream();
+
+        }
+
+        @Override
+        public Spliterator<Entry<K, V>> trySplit() {
+            Spliterator<T> lhsSplit = upstream.trySplit();
+            Spliterator<T> rhsSplit = upstream;
+
+            Spliterator<Entry<K, V>> result;
+
+            if (lhsSplit != null) {
+                // Compute the new tail items from the split; for this purpose
+                // aggregate one group of items from the split
+                Iterator<T> rhsIt = Spliterators.iterator(rhsSplit);
+                Iterator<T> rhsItPlusTail = Iterators.concat(rhsIt, tailItems);
+
+                StreamOperatorSequentialGroupBy<T, K, List<T>>.OperatorImpl op = listAggregator.new OperatorImpl(rhsItPlusTail);
+                Iterator<Entry<K, List<T>>> rhsItPlusTailIt = op.getDownstream();
+
+                List<T> lhsTailItems = Collections.emptyList();
+                // In the worst case this consumes all items from the rhs iterator
+                if (rhsItPlusTailIt.hasNext()) {
+                    lhsTailItems = rhsItPlusTailIt.next().getValue();
+                }
+
+                Iterator<T> rhsHeadItem;
+                Iterator<T> rhsTailItems;
+
+                // Check whether the rhs spliterator has been completely consumed
+                // (togther with the tail items) using the 'lastItemSent' flag
+                // We don't use rhsIt.hasNext() because that might consume another item from rhsSplit
+                if (op.lastItemSent) {
+                    // We have drained the rhs
+                    rhsHeadItem = Collections.<T>emptyList().iterator();
+                    rhsTailItems = Collections.<T>emptyList().iterator();
+                } else {
+                    rhsHeadItem = Collections.singletonList(op.getLastSeenItem()).iterator();
+                    rhsTailItems = tailItems;
+                }
+
+                Iterator<T> lhsHeadItem = headItem;
+
+                // Adjust the tail items of this split
+                this.headItem = rhsHeadItem;
+                this.upstream = rhsSplit;
+                this.tailItems = rhsTailItems;
+                updateIteratorViews();
+
+                result = new SpliteratorImpl(lhsHeadItem, lhsSplit, lhsTailItems.iterator());
+
+            } else {
+                result = null;
+            }
+
+            return result;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Entry<K, V>> action) {
+            boolean result = aggIteratorView.hasNext();
+
+            if (result) {
+                Entry<K, V> entry = aggIteratorView.next();
+                action.accept(entry);
+            }
+
+            return result;
+        }
+    }
+
+
+    public static <T> void split(Spliterator<T> root, Consumer<? super Spliterator<T>> action) {
+        Spliterator<T> rhs = root;
+        Spliterator<T> lhs = root.trySplit();
+
+        if (lhs != null) {
+            System.out.println("LHS");
+            action.accept(lhs);
+        } else {
+            System.out.println("LHS (null)");
+        }
+
+        System.out.println("RHS");
+        action.accept(rhs);
+    }
+
+
+    public static <T> void print(Spliterator<T> root) {
+        Iterator<T> it = Spliterators.iterator(root);
+
+        it.forEachRemaining(System.out::println);
+    }
+
+
     public static void main(String[] args) {
-        Stream<Integer> s = Stream.of(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 1, 2, 3, 4);
 
         StreamOperatorSequentialGroupBy<Integer, Integer, Integer> op =
                 StreamOperatorSequentialGroupBy.create(SequentialGroupBySpec.createAcc(
                         i -> i,
                         () -> 0,
                         (acc, item) -> acc + 1));
+
+
+
+        List<Integer> ints = Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 1, 2, 3, 4);
+
+        // Stream<Integer> s = Stream.of(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 1, 2, 3, 4);
+
+        Spliterator<Integer> root = ints.spliterator();
+
+
+        Spliterator<Entry<Integer, Integer>> derived = op.transform(root);
+
+        // split(derived, StreamOperatorSequentialGroupBy::print);
+
+        // split(derived, x -> split(x, StreamOperatorSequentialGroupBy::print));
+
+        split(derived, x -> split(x, y -> split(y, StreamOperatorSequentialGroupBy::print)));
+
+
+        if (true) {
+            return;
+        }
+
+
+        Stream<Integer> s = ints.stream();
 
 
         List<Entry<Integer, Integer>> actual = op.transform(s).collect(Collectors.toList());
